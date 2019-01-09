911eac2e9ca5b6d56ffe9e91c0f3df0f96a2f98f
Caiqian Cropper <ccropper@c13046ccropper.attlocal.net>
2019-01-09 16:57:13 -0600

    updated robot_name to account for previously used robot names

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -2,11 +2,20 @@ import random
 import string
 
 class Robot(object):
-    def __init__(self):
-        self.name = self.generate_name()
+    used_names = []
     
-    def generate_name(self):
-        return ''.join(random.choices(string.ascii_uppercase, k=2)) + ''.join(random.choices(string.digits, k=3))
+    def __init__(self):
+        self.name = self.generate_unique_name()
+        
+    def generate_unique_name(self):
+        used_name = True
+        while used_name:
+            new_name = ''.join(random.choices(string.ascii_uppercase, k=2)) + ''.join(random.choices(string.digits, k=3))
+            if new_name not in Robot.used_names:
+                used_name = False
+        if used_name == False:
+            Robot.used_names.append(new_name)
+            return new_name
 
     def reset(self):
-        self.name = self.generate_name()
+        self.name = self.generate_unique_name()
