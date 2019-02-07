4b062b7eeccf76da476c87f0577e99cd157a7706
Caiqian Cropper <ccropper@c13046ccropper.attlocal.net>
2019-02-07 12:19:58 -0600

    completed word_count exercise

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,14 +1,7 @@
+import collections
 import re
 
 def word_count(phrase):
-    words_dict = {}
     words_list = re.findall("[0-9a-zA-Z']+", phrase)
-    words_list = [word.lower() for word in words_list]
-    for word in words_list:
-        if word[0] == "'" and word[-1] == "'":
-            word = word [1:-1]
-        if word not in words_dict:
-            words_dict[word] = 1
-        else:
-            words_dict[word] += 1
-    return words_dict
+    words_list = (word.lower().strip("'") for word in words_list)
+    return collections.Counter(words_list)
