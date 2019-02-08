b42f4f370565e5bc4bedf9042879c97b80b4c98d
Caiqian Cropper <ccropper@c13046ccropper.attlocal.net>
2019-02-08 15:56:33 -0600

    hamming exercise: used zip and comprehension based on mentor feedback

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,9 +1,6 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("The lengths of the two strands must be equal.")
-    hamming_distance = 0
-    for base in range(len(strand_a)):
-        if strand_a[base] != strand_b[base]:
-            hamming_distance += 1
-    return hamming_distance   
-
+    strands = zip(strand_a, strand_b)
+    return sum(a != b for a, b in strands)
+    
