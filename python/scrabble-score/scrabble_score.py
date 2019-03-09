631d780629a04e7120f6ce8f5253a05a6978c5c5
Caiqian Cropper <ccropper@c13046ccropper.attlocal.net>
2019-03-09 09:40:00 -0600

    moved LETTER_SCORES outside of function and used a generator instead of for loop for more efficiency

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,12 +1,12 @@
-def score(word):
-    score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+LETTER_SCORES = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
          "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
          "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
          "x": 8, "z": 10}
-    sum = 0
-    for letter in word:
-        sum += score[letter.lower()]
-    return sum
 
-score("apple")
+def score(word):
+    score_sum = 0
+    score_sum = sum(LETTER_SCORES[letter.lower()] for letter in word)
+    return score_sum
+
+score('apple')
